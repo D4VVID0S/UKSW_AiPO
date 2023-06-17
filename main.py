bde9390a6538d923c821e49a2c6574445ca2527e
Matteo Catalani <82810129+D4VVID0S@users.noreply.github.com>
2023-06-17 13:00:16 +0200

    Zadanie wykonane

---
+++ main.py
@@ -0,0 +1,19 @@
+from imageai.Detection import ObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detector = ObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath(
+    os.path.join(execution_path , "retinanet_resnet50_fpn_coco-eeacb38b.pth")
+)
+detector.loadModel()
+detections = detector.detectObjectsFromImage(
+    input_image=os.path.join(execution_path , "005-auto.jpg"),
+    output_image_path=os.path.join(execution_path , "005-auto-nowy.jpg"),
+)
+
+for eachObject in detections:
+    print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+    
